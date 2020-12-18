06f116a67e6d34dcbc6b93a560539f688985b90b
Anthony Kallhoff <akallhoff@tron-akallhoff-mbp.local>
2020-12-18 06:23:27 -1000

    init events table on service_squared db

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -3,42 +3,10 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'postgres',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+      host : '127.0.0.1',
+      database : 'service_squared'
     }
   }
-
 };
---
+++ backend/migrations/20201218061255_events.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex) {
+    return (knex.schema.createTable('events', (table) => {
+        table.increments('id')
+        table.string('name')
+        table.string('description')
+        table.date('start_date')
+        table.time('start_time')
+        table.date('end_date')
+        table.time('end_time')
+        table.timestamp('created_at').defaultTo(knex.fn.now())
+    })
+    )
+};
+
+exports.down = function(knex) {
+    return (knex.schema.dropTableIfExists('events'))
+  
+};
