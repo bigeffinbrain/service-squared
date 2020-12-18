ba89c5cf85764e7675a0c1be1ddd58db71ae5918
Morian Senador <msenador@Morians-MacBook-Pro.local>
2020-12-18 14:53:53 -0500

    Homepage layout basics complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import './App.css';
+import '../src/css files/App.css';
 import { Component } from 'react'
+import EventCard from "./components/eventCard";
+import { BrowserRouter, Route } from "react-router-dom";
+
 
 
 class App extends Component {
   constructor() {
     super()
+    this.state = {
+      events: [
+        {subject:'Event1', info: 'This is Event1', id: 1,},
+        {subject:'Event2', info: 'This is Event1', id: 2},
+        {subject:'Event2', info: 'This is Event1', id: 2},
+        {subject:'Event2', info: 'This is Event1', id: 2},
+        {subject:'Event2', info: 'This is Event1', id: 2},
+        {subject:'Event2', info: 'This is Event1', id: 2},
+        
+      ],
+      activeEvent: null
+    }
   }
   
   render() {
     return(
       <>
-        <h1>This is an app</h1>
+        <header class="main-header">
+          <button id="home-button">Home</button>
+          <h1 id="main-title">Welcome to Service Squared</h1>
+          <h6 id="main-title">Since: 2020</h6>
+        </header>
+        <div class="create-event-area">
+          <button id="create-event-button">Create Event</button>
+          <h3>These events need your help! Check them out:</h3>
+        </div>
+        <div class="events-elements-grid">
+          <EventCard allCards={this.state.events}/>
+        </div>
       </>
     )
   }
