e48e8de2f49ed5c1f9bc7af7a24c7af8530c4e3f
Morian Senador <msenador@Morians-MacBook-Pro.local>
2020-12-18 11:28:53 -0500

    initial front-end

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { Component } from 'react'
+
+
+class App extends Component {
+  constructor() {
+    super()
+  }
+  
+  render() {
+    return(
+      <>
+        <h1>This is an app</h1>
+      </>
+    )
+  }
+}
+
+export default App;
