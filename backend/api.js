1601f534459daa11163acc5d33809a1c5d7bcdb5
Cecil Montemayor <cecilmontemayoriv@gmail.com>
2020-12-21 17:42:14 -0500

    added cors to express server, added submit handler to create event form

--- backend/api.js
+++ backend/api.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3001;
+const cors = require('cors')
+const parser = require('body-parser')
+
+//cors policy update
+app.use(cors())
+app.use(parser.json())
 
 //Root for api server connection testing
 app.get('/', (request, response) => {
@@ -9,7 +15,9 @@ app.get('/', (request, response) => {
 
 // post for setting values to an event
 app.post('/create-event', (request, response) => {
-  
+  let reqbody = request.body
+  console.log(reqbody)
+  response.status(200)
 })
 
 app.listen(port, () => console.log(`Server up and running @ Localhost:${port}`))
--- src/components/createEvent.js
+++ src/components/createEvent.js
@@ -1,19 +1,75 @@
 import React from 'react'
 import '../css files/cards.css'
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 
-const CreateEvent = () => {
-    return(
-        <div class="create-event-form">
-            <form id="event-form">
-                <input id="name-input" class="inputField" placeholder="Event Name"/>
-                <input id="desc-input" class="inputField" placeholder="Description"/>
-                <input id="start-input" class="inputField" type="date"/>
-                <input id="end-input" class="inputField" type="date"/>
-                <button id="submit-event" type="submit">Submit</button>
-            </form>
-        </div>
-    )
+class CreateEvent extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+            description: '',
+            startDate: '',
+            endDate: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        let nam = event.target.name
+        let val = event.target.value
+
+        this.setState({
+            [nam]: val
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        fetch('http://127.0.0.1:3001/create-event', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(this.state),
+          })
+          .then(promise => promise.json())
+          .then(data => {
+            console.log('Success:', data);
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          })
+    }
+
+    render() {
+        return (
+            <div class="create-event-form">
+                <form id="event-form" onSubmit={this.handleSubmit}>
+                    <input id="name-input" name="name" class="inputField" type="text" placeholder="Event Name" onChange={this.handleChange}/>
+                    <input id="desc-input" name="description" class="inputField" type="text" placeholder="Description" onChange={this.handleChange}/>
+                    <input id="start-input" name="startDate" class="inputField" type="date" onChange={this.handleChange}/>
+                    <input id="end-input" name="endDate" class="inputField" type="date" onChange={this.handleChange}/>
+                    <button id="submit-event" type="submit">Submit</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default CreateEvent;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
