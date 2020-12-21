64c9747ebd3eecdc59f009aac77c60285dd4b321
Cecil Montemayor <cecilmontemayoriv@gmail.com>
2020-12-21 14:38:33 -0500

    added create-event endpoint

--- backend/api.js
+++ backend/api.js
@@ -3,9 +3,13 @@ const app = express()
 const port = 3001;
 
 //Root for api server connection testing
-app.get('/',(request,response) =>{
+app.get('/', (request, response) => {
   response.status(200).send('Successful')
 })
 
+// post for setting values to an event
+app.post('/create-event', (request, response) => {
+  
+})
 
-app.listen(port,() => console.log(`Server up and running @ Localhost:${port}`))
+app.listen(port, () => console.log(`Server up and running @ Localhost:${port}`))
